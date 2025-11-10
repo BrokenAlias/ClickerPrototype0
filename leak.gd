609507d157a3adcf2baac00f4d317a8de4d48c58
BrokenAlias <mathias.berntsen@gmail.com>
2025-11-10 22:28:25 +0100

    Added pipe leak fix minigame

---
+++ leak.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@export var max_size: float = 20.0
+@export var min_size: float = 10.0
+@export var shrink_amount: float = 3.0
+@export var regrow_speed: float = 1.0
+
+var current_size: float
+var hits: int = 0
+
+@onready var rect = $SpriteRect
+
+func _ready() -> void:
+	current_size = max_size
+	# size is a Vector2
+	rect.size = Vector2(current_size, current_size)
+	# ensure the ColorRect accepts mouse input (you can also set this in the editor)
+	rect.mouse_filter = Control.MOUSE_FILTER_STOP
+	
+func _process(delta: float) -> void:
+	# Slowly regrow if smaller
+	if current_size < max_size:
+		current_size += regrow_speed * delta
+		current_size = min(current_size, max_size)
+		rect.size = Vector2(current_size, current_size)
+		
+func _on_sprite_rect_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		shrink()
+		
+func shrink():
+	hits += 1 
+	current_size = max(min_size, current_size - shrink_amount)
+	rect.size = Vector2(current_size, current_size)
+	
+	if hits >= 3:
+		queue_free()
+		get_parent().emit_signal("leak_fixed")
---
+++ scenes/pipes.gd
@@ -0,0 +1,42 @@
+extends Control
+
+@onready var pipe_area: ColorRect = $PipeArea
+@onready var bar = $PipePressure
+
+@export var leak_scene: PackedScene
+@export var spawn_interval = 2.0
+
+var spawn_timer = 0.0
+
+signal leak_fixed
+
+func _ready():
+	bar.value = 0
+	connect("leak_fixed", Callable(self, "_on_leak_fixed"))
+
+	
+func _process(delta: float) -> void:
+	spawn_timer += delta
+	if spawn_timer >= spawn_interval:
+		spawn_timer = 0
+		spawn_leak()
+		
+
+func spawn_leak():
+	var leak = leak_scene.instantiate()
+	add_child(leak)
+	
+	
+	# random posiston inside PipeArea
+	var rect_size = pipe_area.size
+
+	leak.position = Vector2(
+		randf_range(0, rect_size.x),
+		randf_range(0, rect_size.y)
+	)
+
+func _on_leak_fixed():
+	bar.value += 10
+	if bar.value >= 100:
+		print("Pipe Fixed")
+		hide()
